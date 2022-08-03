bb263f932b065c376f5cd8b43b03a82128b5adf0
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2022-08-03 16:56:44 +0900

    [Init] test isnan -> OK

---
+++ test/test_nnabla/func/test_nnabla_func_isnan.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file test_nnabla_func_isnan.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+// googletest
+#include <gtest/gtest.h>
+
+// nnabla
+#include <test_nnabla.hpp>
+
+// test utils
+#include <test_utils.hpp>
+
+
+namespace
+{
+     TEST(nnabla_func, isnan_1)
+    {
+        using namespace std;
+        const auto& ctx_cpu = SingletonManager::get<GlobalContext>()->get_current_context();
+
+        // zero
+        auto ndarray_zeros = zeros<float>(Shape_t{3, 3});
+        auto st = ndarray_zeros->strides();
+        auto sh = ndarray_zeros->shape();
+        float* data = ndarray_zeros->cast_data_and_get_pointer<float>(ctx_cpu);
+
+        data[0 * st[0] + 0 * st[1]] = 1.5;
+        data[1 * st[0] + 1 * st[1]] = std::numeric_limits<float>::quiet_NaN();
+        data[2 * st[0] + 2 * st[1]] = std::numeric_limits<float>::signaling_NaN();
+
+        show_ndarray_contents<float>(ndarray_zeros);
+
+        cout << "---" <<endl;
+
+        auto out_ndarray = is::common::invoke_tm_chrono_ms_ret(is::nbla::isnan<float>, ndarray_zeros);
+
+        show_ndarray_contents<bool>(out_ndarray);
+    }
+}
+
+int main(int, char**)
+{
+    std::cout << __FILE__ << std::endl;
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
