ab850d36352e5bfee0525f99ec52431efead5725
Inoue Shinichi for Windows Desktop <inoue.shinichi.1800@gmail.com>
2022-08-05 22:44:16 +0900

    [Update] IS_DEBUG_STREAMの変更

--- include/IsCommon/IsCommon.hpp
+++ include/IsCommon/IsCommon.hpp
@@ -29,7 +29,26 @@
 // debug log
 #if 1
     #include <cstdio>
-    #define IS_DEBUG_STREAM(format, ...) std::printf(format, ##__VA_ARGS__)
+    #if __cplusplus <= 201703L // <= C++17
+        #define IS_DEBUG_STREAM(format, ...) std::printf(format, ##__VA_ARGS__)
+    #else // >= C++20
+        #ifdef _MSC_VER
+            #if _MSC_VER >= 1925 // >= Visual Studio 2019 16.5.1
+                #define IS_DEBUG_STREAM(format, ...) std::printf(format __VA_OPT__(,) __VA_ARGS__)
+            #else // < Visual Studio 2019 16.5.1
+                // _MSVC_TRADITIONAL 従来:1, 準拠:0
+                #if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL // 従来型プリプロセッサ
+                    #define IS_EXPAND(...) __VA_ARGS__
+                    #define IS_DEBUG_STREAM(format, ...) std::printf(format IS_EXPAND(__VA_ARGS__))
+                    #undef IS_EXPAND
+                #else // 新型プリプロセッサ
+                    #define IS_DEBUG_STREAM(format, ...) std::printf(format, ##__VA_ARGS__)
+                #endif
+            #endif
+        #else // GCC or Clang (>= C++20)
+            #define IS_DEBUG_STREAM(format, ...) std::printf(format __VA_OPT__(,) __VA_ARGS__)
+        #endif
+    #endif
 #else
     #define IS_DEBUG_STREAM(format, ...)
 #endif
