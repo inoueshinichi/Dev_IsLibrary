371da7dfad741dd0922fa46d9bc32d1b9b4ec68d
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2023-02-02 01:23:52 +0900

    [Update] bug fix

--- include/IsCommon/internal/defs.hpp
+++ include/IsCommon/internal/defs.hpp
@@ -85,7 +85,7 @@ using uint = unsigned int;
         using uint32 = unsigned int;
         using int64 = long;
         using uint64 = ulong;
-    #elseif defined(__MACH__)  // macOS
+    #elif defined(__MACH__)  // macOS
         using int32 = int;
         using uint32 = unsigned int;
         using int64 = longlong;
