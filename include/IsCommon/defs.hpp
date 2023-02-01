b5ea0464b7065a615b388c26883e0d12f7f9034f
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2023-02-02 00:04:42 +0900

    [Update] modify primitive type alias

--- include/IsCommon/defs.hpp
+++ include/IsCommon/defs.hpp
@@ -73,9 +73,18 @@ using uint16 = unsigned short;
 
 using uint = unsigned int;
 
-using int32 = int;
-using uint32 = unsigned int;
-using int64 = long long;
-using uint64 = unsigned long long;
+#if defined(_WIN64) && defined(_WIN32) // 32bit環境
+    using int32 = long;
+    using uint32 = ulong;
+    using int64 = longlong;
+    using uint64 = ulonglong;
+#else
+    using int32 = int;
+    using uint32 = unsigned int;
+    using int64 = long;
+    using uint64 = ulong;
+#endif
+
+
 
 using longdouble = long double;
