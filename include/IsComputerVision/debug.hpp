ff310ff1a753e86f79ca2c559dd6f4013dd3df13
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-02-04 20:39:28 +0900

    [Update] mod ndarray checker

--- include/IsComputerVision/debug.hpp
+++ include/IsComputerVision/debug.hpp
@@ -12,77 +12,68 @@
 
 #include <IsComputerVision/IsComputerVision.hpp>
 
+#if (!IS_DEBUG_FLAG)
+    #define IS_DEBUG_NDARRAY_STATE(ndarray, type, ndim, shape, strides)
+#else
+    #define IS_DEBUG_NDARRAY_STATE(ndarray, type, ndim, shape, strides) \
+        std::ostringstream oss;                                         \
+        oss << "[" << #ndarray << "]: ";                                \
+        oss << "type " << type << ", ";                                 \
+        oss << "ndim " << ndim << ", ";                                 \
+        oss << "shape (";                                               \
+        for (int d = 0; d < ndim; ++d)                                  \
+        {                                                               \
+            oss << shape[d];                                            \
+            if (d != ndim - 1)                                          \
+            {                                                           \
+                oss << ",";                                             \
+            }                                                           \
+            else                                                        \
+            {                                                           \
+                oss << "), ";                                           \
+            }                                                           \
+        }                                                               \
+                                                                        \
+        oss << "strides (";                                             \
+        for (int32 d = 0; d < ndim; ++d)                                \
+        {                                                               \
+            oss << strides[d];                                          \
+            if (d != ndim - 1)                                          \
+            {                                                           \
+                oss << ",";                                             \
+            }                                                           \
+            else                                                        \
+            {                                                           \
+                oss << ")";                                             \
+            }                                                           \
+        }                                                               \
+        oss << " at " << __func__ << " in ";                            \
+        oss << __FILE__ << ":" << __LINE__;                             \
+        std::cout << oss.str() << std::endl;
+#endif
 
-#define IS_DEBUG_CHECK_NDARRAY_STATE(func, flag, ndarray) \
-    if (flag > 0)                                         \
-    {                                                     \
-        auto ndim = ndarray->ndim();                      \
-        auto shape = ndarray->shape();                    \
-        auto strides = ndarray->strides();                \
-        auto dtype = ndarray->dtype();                    \
-        auto type = nbla::dtype_to_string(dtype);         \
-        std::ostringstream oss;                           \
-        oss << #func << ", ";                             \
-        oss << "Type: " << type << ", ";                  \
-        oss << "ndim: " << ndim << ", ";                  \
-        oss << "shape: (";                                \
-        for (int32 d = 0; d < ndim; ++d)                  \
-        {                                                 \
-            oss << shape[d];                              \
-            if (d != ndim - 1)                            \
-            {                                             \
-                oss << ",";                               \
-            }                                             \
-            else                                          \
-            {                                             \
-                oss << "), ";                             \
-            }                                             \
-        }                                                 \
-                                                          \
-        oss << "strides: (";                              \
-        for (int32 d = 0; d < ndim; ++d)                  \
-        {                                                 \
-            oss << strides[d];                            \
-            if (d != ndim - 1)                            \
-            {                                             \
-                oss << ",";                               \
-            }                                             \
-            else                                          \
-            {                                             \
-                oss << ")";                               \
-            }                                             \
-        }                                                 \
-        std::cout << oss.str() << std::endl;              \
-    }
-
-
-#define IS_CHECK_NDARRAY_SHAPE_AS_IMAGE(ndarray)                                       \
+#define IS_DEBUG_NDARRAY_SHAPE_AS_IMAGE(ndarray)                                       \
     {                                                                                  \
-        if (!ndarray.get())                                                            \
-        {                                                                              \
-            throw std::runtime_error("NdArray is nullptr.");                           \
-        }                                                                              \
         auto ndim = ndarray->ndim();                                                   \
         auto shape = ndarray->shape();                                                 \
         auto strides = ndarray->strides();                                             \
-                                                                                       \
+        auto dtype = ndarray->dtype();                                                 \
+        auto type = nbla::dtype_to_string(dtype);                                      \
+        IS_DEBUG_NDARRAY_STATE(ndarray, type, ndim, shape, strides)                    \
         if (ndim != 3)                                                                 \
         {                                                                              \
-            throw std::runtime_error(                                                  \
-                is::common::format_string("Miss matching 3 ndim. Given is %d", ndim)); \
+            throw IS_ERROR(error_code::value, "%s is not 3 dimension. Given is %d.",   \
+                           #ndarray, ndim);                                            \
         }                                                                              \
         else                                                                           \
         {                                                                              \
-            /*(C, H, W)*/                                                              \
-            int32 channels = shape[0];                                                 \
+            /*(H,W,C)*/                                                                \
+            int channels = shape[2];                                                   \
             if (channels > 3 || channels == 2)                                         \
             {                                                                          \
-                throw std::runtime_error(is::common::format_string(                    \
-                    "Miss matching channels of top shape (<=3 or ==2). Given is %d",   \
-                    channels));                                                        \
+                throw IS_ERROR(error_code::value,                                      \
+                               "Channels(shape[2]) of %s must be 1 or 3. Given is %d", \
+                               #ndarray, channels));                                   \
             }                                                                          \
         }                                                                              \
     }
-
-
-
