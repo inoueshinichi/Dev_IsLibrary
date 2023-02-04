46bdd0e816f881dc79b343275b5d334262f3722e
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-02-04 20:41:23 +0900

    [Update] change alias primitive type

--- include/IsCommon/internal/defs.hpp
+++ include/IsCommon/internal/defs.hpp
@@ -64,6 +64,7 @@ using uchar = unsigned char;
 using byte = char;
 using ubyte = unsigned char;
 using ushort = unsigned short;
+using uint = unsigned int;
 using ulong = unsigned long;
 using longlong = long long;
 using ulonglong = unsigned long long;
@@ -71,30 +72,28 @@ using ulonglong = unsigned long long;
 using int16 = short;
 using uint16 = unsigned short;
 
-using uint = unsigned int;
-
 
 #if defined(_WIN64) && defined(_WIN32) // 32bit環境
     using int32 = long;
-    using uint32 = ulong;
-    using int64 = longlong;
-    using uint64 = ulonglong;
+    using uint32 = unsigned long;
+    using int64 =  long long;
+    using uint64 = unsigned long long;
 #else
     #if defined(__linux__) // Linux
         using int32 = int;
         using uint32 = unsigned int;
-        using int64 = long;
-        using uint64 = ulong;
+        using int64 = long long;
+        using uint64 = unsigned long long;
     #elif defined(__MACH__)  // macOS
         using int32 = int;
         using uint32 = unsigned int;
-        using int64 = longlong;
-        using uint64 = ulonglong;
+        using int64 = long long;
+        using uint64 = unsigned long long;
     #else
         using int32 = int;
         using uint32 = unsigned int;
-        using int64 = longlong;
-        using uint64 = ulonglong;
+        using int64 = long long;
+        using uint64 = unsigned long long;
     #endif
 #endif
 
